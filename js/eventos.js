3ec90fc974e437d03af33c691a85387b2b88fb06
veronicaxvnf <xvnf@hotmail.com>
2023-02-27 18:14:12 -0300

    subir proyecto

---
+++ js/eventos.js
@@ -0,0 +1,7 @@
+
+document.getElementById('boton').onclick = function () {
+    console.log("capturamos el evento click");
+    this.innerHTML = "Gracias"
+}
+
+/* Fue muy interesante el curso, seria mejor con una tutoria on line.Los colores en el CV fueron para poder diferenciar en el codigo la ubicacion y no perderme*/
